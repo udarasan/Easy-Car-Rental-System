0974dcb7212ce312e1a2bd46912fd2057d0b5456
udarasan <udarassanjeewa@gmail.com>
2021-06-13 17:49:26 +0530

    JPAConfig File Initialize

--- BackEnd/src/main/java/lk/ijse/easycarrentalsystem/config/WebRootConfig.java
+++ BackEnd/src/main/java/lk/ijse/easycarrentalsystem/config/WebRootConfig.java
@@ -1,7 +1,9 @@
 package lk.ijse.easycarrentalsystem.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import({JPAConfig.class})
 public class WebRootConfig {
 }
